a9f666973aea0d3666d611890c0333fd78dca507
Karen <chikis.junior.karen@gmail.com>
2021-12-03 10:19:00 -0500

    primer commit

---
+++ InicialB/apps.js
@@ -0,0 +1,25 @@
+/*1. Mostrar todos los animales cuya inicial sea la letra B*/
+
+const animales = [
+    { name: "Lobo", type: "Mamifero", age: 5, color: "Gris" },
+    { name: "Zorro", type: "Mamifero", age: 2, color: "Amarillo" },
+    { name: "Gato", type: "Mamifero", age: 7, color: "Negro" },
+    { name: "Caballo", type: "Mamifero", age: 3, color: "Café" },
+    { name: "León", type: "Mamifero", age: 12, color: "Café" },
+    { name: "Elefante", type: "Mamifero", age: 13, color: "Gris" },
+    { name: "Jirafa", type: "Mamifero", age: 9, color: "Amarillo" },
+    { name: "Tigre", type: "Mamifero", age: 5, color: "Amarillo" },
+    { name: "Ballena", type: "Mamifero", age: 3, color: "Azul" },
+    { name: "Búho", type: "Ave", age: 13, color: "Café" },
+    { name: "Gallina", type: "Ave", age: 9, color: "Blanco" },
+    { name: "Águila", type: "Ave", age: 4, color: "Rojo" },
+    { name: "Perro", type: "Mamifero", age: 5, color: "Blanco" },
+    { name: "Toro", type: "Mamifero", age: 12, color: "Negro" },
+    { name: "Oveja", type: "Mamifero", age: 13, color: "Blanco" },
+    { name: "Hombre", type: "Mamifero", age: 9, color: "Piel" },
+];
+
+
+const filtrar = animales.filter(nombre => nombre.name[0] === 'B')
+
+console.log(filtrar)
